55411b406ac5419b143856d3df4050941af39114
Ian H <ian@irhconsulting.co.uk>
2021-03-15 17:32:17 +0100

    adding web handler for when app isnt installed

--- back-end/src/functions/nfcTest.ts
+++ back-end/src/functions/nfcTest.ts
@@ -19,23 +19,13 @@ export const handler: APIGatewayProxyHandler = async (
     "Content-Type": "application/json",
   };
 
-  const assetLinks = [
-    {
-      relation: ["delegate_permission/common.handle_all_urls"],
-      target: {
-        namespace: "android_app",
-        package_name: "com.rnnfcreaderspike",
-        sha256_cert_fingerprints: [
-          "FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C",
-        ],
-      },
-    },
-  ];
+  const { pathParameters } = event;
+  const code = pathParameters?.code ?? "";
 
   const response: APIGatewayProxyResult = {
     statusCode: 200,
 
-    body: JSON.stringify(assetLinks),
+    body: code,
     headers: headers,
   };
 
